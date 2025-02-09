a7f15e4a357f0fbcb42ca6c54c2843b851508f02
ThanusTK <65088712+ThanusTK@users.noreply.github.com>
2025-02-08 21:42:13 -0800

    Update TikTokLiveOfflineHostException.java
    Se Retiro Mensaje para Consolas Java

--- API/src/main/java/io/github/jwdeveloper/tiktok/exceptions/TikTokLiveOfflineHostException.java
+++ API/src/main/java/io/github/jwdeveloper/tiktok/exceptions/TikTokLiveOfflineHostException.java
@@ -25,6 +25,6 @@ package io.github.jwdeveloper.tiktok.exceptions;
 public class TikTokLiveOfflineHostException extends TikTokLiveException
 {
     public TikTokLiveOfflineHostException(String message) {
-        super(message);
+        //super(message);
     }
 }
